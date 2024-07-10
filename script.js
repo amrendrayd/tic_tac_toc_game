0d9f9b41de20792d7093abee59ff48b7d327d7a7
amrendrayd <amrendrayd3@gmail.com>
2024-07-10 19:33:09 +0530

    Complete our Tic Tac Toc Game

---
+++ script.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let trunO = true; 
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+const resetGame = () => {
+    trunO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    // disableBoxes();
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        if(trunO) {
+            box.innerText = "O";
+            trunO = false;
+        } else {
+            box.innerText = "X";
+            trunO = true;
+        }
+        box.disabled = true
+
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerHTML = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame)
